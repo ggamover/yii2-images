df1831d7682786727442dc58bda663d91117bcdb
Oleh <awalonweb@gmail.com>
2023-01-09 14:33:46 +0200

    Fixed issue with "File not found" when record in db exists but file is missing

--- models/Image.php
+++ models/Image.php
@@ -16,7 +16,6 @@ namespace rico\yii2images\models;
 
 use Imagick;
 use ImagickException;
-use Yii;
 use yii\base\ErrorException;
 use yii\base\Exception;
 use yii\db\ActiveRecord;
@@ -37,304 +36,261 @@ use claviska\SimpleImage;
  * @property string filePath
  * @property string modelName
  * @property string name
+ * @property int isMain
  */
 class Image extends ActiveRecord
 {
-    use ModuleTrait;
+	use ModuleTrait;
 
+	const GRAPHICS_LIB_IMAGICK = 'Imagick';
 
 	/**
 	 * @var bool
 	 */
 	private $helper = false;
 
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName(): string
+	{
+		return '{{%image}}';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function rules(): array
+	{
+		return [
+			[['filePath', 'itemId', 'modelName', 'urlAlias'], 'required'],
+			[['itemId', 'isMain'], 'integer'],
+			[['name'], 'string', 'max' => 80],
+			[['filePath', 'urlAlias'], 'string', 'max' => 400],
+			[['modelName'], 'string', 'max' => 150]
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels(): array
+	{
+		return [
+			'id' => 'ID',
+			'filePath' => 'File Path',
+			'itemId' => 'Item ID',
+			'isMain' => 'Is Main',
+			'modelName' => 'Model Name',
+			'urlAlias' => 'Url Alias',
+		];
+	}
 
 	/**
 	 * @return bool
 	 * @throws Exception
 	 * @throws ErrorException
 	 */
-	public function clearCache(){
-        $subDir = $this->getSubDur();
+	public function clearCache(): bool
+	{
+		$subDir = $this->getSubDur();
+		$dirToRemove = $this->getModule()->getCachePath().DIRECTORY_SEPARATOR.$subDir;
+		if(preg_match('/'.preg_quote($this->modelName, '/').'/', $dirToRemove)){
+			BaseFileHelper::removeDirectory($dirToRemove);
+		}
 
-        $dirToRemove = $this->getModule()->getCachePath().DIRECTORY_SEPARATOR.$subDir;
-
-        if(preg_match('/'.preg_quote($this->modelName, '/').'/', $dirToRemove)){
-            BaseFileHelper::removeDirectory($dirToRemove);
-
-        }
-
-        return true;
-    }
+		return true;
+	}
 
 	/**
 	 * @return array|string|string[]
+	 * @throws Exception
 	 */
 	public function getExtension(){
-        $ext = pathinfo($this->getPathToOrigin(), PATHINFO_EXTENSION);
-        return $ext;
-    }
+		return pathinfo($this->getPathToOrigin(), PATHINFO_EXTENSION);
+	}
 
 	/**
-	 * @param $size
-	 * @return string
-	 */
-	public function getUrl($size = false){
-        $urlSize = ($size) ? '_'.$size : '';
-        $url = Url::toRoute([
-            '/'.$this->getPrimaryKey().'/images/image-by-item-and-alias',
-            'item' => $this->modelName.$this->itemId,
-            'dirtyAlias' =>  $this->urlAlias.$urlSize.'.'.$this->getExtension()
-        ]);
-
-        return $url;
-    }
-
-	/**
-	 * @param $size
+	 * @param bool $size
 	 * @return string
 	 * @throws Exception
 	 */
-	public function getPath($size = false){
-        $urlSize = ($size) ? '_'.$size : '';
-        $base = $this->getModule()->getCachePath();
-        $sub = $this->getSubDur();
+	public function getUrl(bool $size = false): string
+	{
+		$urlSize = ($size) ? '_'.$size : '';
+		return Url::toRoute([
+			'/'.$this->getPrimaryKey().'/images/image-by-item-and-alias',
+			'item' => $this->modelName.$this->itemId,
+			'dirtyAlias' =>  $this->urlAlias.$urlSize.'.'.$this->getExtension()
+		]);
+	}
 
-        $origin = $this->getPathToOrigin();
+	/**
+	 * @param bool $size
+	 * @return string
+	 * @throws Exception|ImagickException
+	 * @throws \Exception
+	 */
+	public function getPath($size = false): string
+	{
+		$urlSize = ($size) ? '_'.$size : '';
+		$base = $this->getModule()->getCachePath();
+		$sub = $this->getSubDur();
 
-        $filePath = $base.DIRECTORY_SEPARATOR.
-            $sub.DIRECTORY_SEPARATOR.$this->urlAlias.$urlSize.'.'.pathinfo($origin, PATHINFO_EXTENSION);
+		$origin = $this->getPathToOrigin();
+		$filePath = $this->getFilePath($base, $sub, $urlSize, $origin);
+
 		if(!file_exists($filePath)){
-            $this->createVersion($origin, $size);
+			$this->createVersion($origin, $size);
+			if(!file_exists($filePath)){
+				throw new \Exception('Problem with image creating.');
+			}
+		}
 
-            if(!file_exists($filePath)){
-                throw new \Exception('Problem with image creating.');
-            }
-        }
-
-        return $filePath;
-    }
+		return $filePath;
+	}
 
 	/**
-	 * @param $size
+	 * @param string $base - base cache path
+	 * @param string $imageSubDir - image sub directory
+	 * @param string $widthHeight - image size width and heihgt from url path
+	 * @param string $sourcePath - image source path
 	 * @return false|string
-	 * @throws Exception
+	 * @throws Exception|ImagickException
 	 */
-	public function getContent($size = false){
-        return file_get_contents($this->getPath($size));
-    }
+	protected function getFilePath($cachePath, $imageSubDir, $widthHeight, $sourcePath): string
+	{
+		return $cachePath.DIRECTORY_SEPARATOR.
+			$imageSubDir.
+			DIRECTORY_SEPARATOR.
+			$this->urlAlias.
+			$widthHeight.'.'.pathinfo($sourcePath, PATHINFO_EXTENSION);
+	}
+
+	/**
+	 * @param bool $size
+	 * @return false|string
+	 * @throws Exception|ImagickException
+	 */
+	public function getContent(bool $size = false){
+		return file_get_contents($this->getPath($size));
+	}
 
 	/**
 	 * @return string
 	 * @throws Exception
 	 */
-	public function getPathToOrigin(){
-
-        $base = $this->getModule()->getStorePath();
-
-        $filePath = $base.DIRECTORY_SEPARATOR.$this->filePath;
-
-        return $filePath;
-    }
-
+	public function getPathToOrigin(): string
+	{
+		$base = $this->getModule()->getStorePath();
+		return $base.DIRECTORY_SEPARATOR.$this->filePath;
+	}
 
 	/**
 	 * @return false|int[]
 	 * @throws Exception
 	 * @throws ImagickException
+	 * @throws \Exception
 	 */
 	public function getSizes()
-    {
-        $sizes = false;
-        if($this->getModule()->graphicsLibrary == 'Imagick'){
-            $image = new Imagick($this->getPathToOrigin());
-            $sizes = $image->getImageGeometry();
-        }else{
-            $image = new SimpleImage($this->getPathToOrigin());
-            $sizes['width'] = $image->get_width();
-            $sizes['height'] = $image->get_height();
-        }
+	{
+		$sizes = false;
+		if($this->getModule()->graphicsLibrary == static::GRAPHICS_LIB_IMAGICK){
+			$image = new Imagick($this->getPathToOrigin());
+			$sizes = $image->getImageGeometry();
+		}else{
+			$image = new SimpleImage($this->getPathToOrigin());
+			$sizes['width'] = $image->getWidth();
+			$sizes['height'] = $image->getHeight();
+		}
 
-        return $sizes;
-    }
-
-	/**
-	 * @param $sizeString
-	 * @return array
-	 * @throws Exception
-	 * @throws ImagickException
-	 */
-	public function getSizesWhen($sizeString){
-
-        $size = $this->getModule()->parseSize($sizeString);
-        if(!$size){
-            throw new \Exception('Bad size..');
-        }
-
-
-
-        $sizes = $this->getSizes();
-
-        $imageWidth = $sizes['width'];
-        $imageHeight = $sizes['height'];
-        $newSizes = [];
-        if(!$size['width']){
-            $newWidth = $imageWidth*($size['height']/$imageHeight);
-            $newSizes['width'] = intval($newWidth);
-            $newSizes['height'] = $size['height'];
-        }elseif(!$size['height']){
-            $newHeight = intval($imageHeight*($size['width']/$imageWidth));
-            $newSizes['width'] = $size['width'];
-            $newSizes['height'] = $newHeight;
-        }
-
-        return $newSizes;
-    }
+		return $sizes;
+	}
 
 	/**
 	 * @param $imagePath
-	 * @param $sizeString
+	 * @param string|null $sizeString
 	 * @return SimpleImage|Imagick
 	 * @throws Exception
 	 * @throws ImagickException
+	 * @throws \Exception
 	 */
-	public function createVersion($imagePath, $sizeString = false)
-    {
-        if(strlen($this->urlAlias)<1){
-            throw new \Exception('Image without urlAlias!');
-        }
+	public function createVersion($imagePath, $sizeString = null)
+	{
+		if(strlen($this->urlAlias)<1){
+			throw new \Exception('Image without urlAlias!');
+		}
 
-        $cachePath = $this->getModule()->getCachePath();
-        $subDirPath = $this->getSubDur();
-        $fileExtension =  pathinfo($this->filePath, PATHINFO_EXTENSION);
+		$cachePath = $this->getModule()->getCachePath();
+		$subDirPath = $this->getSubDur();
+		$fileExtension =  pathinfo($this->filePath, PATHINFO_EXTENSION);
+		$sizePart = $sizeString ? '_'.$sizeString : '';
 
-        if($sizeString){
-            $sizePart = '_'.$sizeString;
-        }else{
-            $sizePart = '';
-        }
+		$pathToSave = $cachePath.'/'.$subDirPath.'/'.$this->urlAlias.$sizePart.'.'.$fileExtension;
+		BaseFileHelper::createDirectory(dirname($pathToSave), 0777, true);
 
-        $pathToSave = $cachePath.'/'.$subDirPath.'/'.$this->urlAlias.$sizePart.'.'.$fileExtension;
+		$size = $sizeString ? $this->getModule()->parseSize($sizeString): false;
 
-        BaseFileHelper::createDirectory(dirname($pathToSave), 0777, true);
+		if($this->getModule()->graphicsLibrary == static::GRAPHICS_LIB_IMAGICK){
+			$image = new Imagick($imagePath);
+			$image->setImageCompressionQuality( $this->getModule()->imageCompressionQuality );
+			if($size){
+				if($size['height'] && $size['width']){
+					$image->cropThumbnailImage($size['width'], $size['height']);
+				}elseif($size['height']){
+					$image->thumbnailImage(0, $size['height']);
+				}elseif($size['width']){
+					$image->thumbnailImage($size['width'], 0);
+				}else{
+					throw new \Exception('Something wrong with this->module->parseSize($sizeString)');
+				}
+			}
 
+			$image->writeImage($pathToSave);
+		}else{
+			$image = new SimpleImage($imagePath);
+			if($size){
+				if($size['height'] && $size['width']){
+					$image->thumbnail($size['width'], $size['height']);
+				}elseif($size['height']){
+					$image->resize(null, $size['height']);
+				}elseif($size['width']){
+					$image->resize($size['width']);
+				}else{
+					throw new \Exception('Something wrong with this->module->parseSize($sizeString)');
+				}
+			}
+			$image->toFile($pathToSave, null, $this->getModule()->imageCompressionQuality );
+		}
 
-        if($sizeString) {
-            $size = $this->getModule()->parseSize($sizeString);
-        }else{
-            $size = false;
-        }
-
-            if($this->getModule()->graphicsLibrary == 'Imagick'){
-                $image = new Imagick($imagePath);
-
-                $image->setImageCompressionQuality( $this->getModule()->imageCompressionQuality );
-
-                if($size){
-                    if($size['height'] && $size['width']){
-                        $image->cropThumbnailImage($size['width'], $size['height']);
-                    }elseif($size['height']){
-                        $image->thumbnailImage(0, $size['height']);
-                    }elseif($size['width']){
-                        $image->thumbnailImage($size['width'], 0);
-                    }else{
-                        throw new \Exception('Something wrong with this->module->parseSize($sizeString)');
-                    }
-                }
-
-                $image->writeImage($pathToSave);
-            }else{
-
-                $image = new SimpleImage($imagePath);
-
-                if($size){
-                    if($size['height'] && $size['width']){
-
-                        $image->thumbnail($size['width'], $size['height']);
-                    }elseif($size['height']){
-                        $image->fit_to_height($size['height']);
-                    }elseif($size['width']){
-                        $image->fit_to_width($size['width']);
-                    }else{
-                        throw new \Exception('Something wrong with this->module->parseSize($sizeString)');
-                    }
-                }
-
-                $image->toFile($pathToSave, null, $this->getModule()->imageCompressionQuality );
-            }
-
-        return $image;
-
-    }
+		return $image;
+	}
 
 
 	/**
-	 * @param $isMain
+	 * @param bool $isMain
 	 * @return void
 	 */
-	public function setMain($isMain = true){
-        if($isMain){
-            $this->isMain = 1;
-        }else{
-            $this->isMain = 0;
-        }
-
-    }
-
+	public function setMain(bool $isMain = true)
+	{
+		$this->isMain = $isMain;
+	}
 
 	/**
-	 * @param $size
+	 * @param bool $size
 	 * @return string
-	 * @throws Exception
+	 * @throws Exception|ImagickException
 	 */
-	public function getMimeType($size = false) {
-        return image_type_to_mime_type ( exif_imagetype( $this->getPath($size) ) );
-    }
-
+	public function getMimeType(bool $size = false): string
+	{
+		return image_type_to_mime_type ( exif_imagetype( $this->getPath($size) ) );
+	}
 
 	/**
 	 * @return string
 	 */
-	protected function getSubDur(){
-        return Inflector::pluralize($this->modelName).'/'.$this->modelName.$this->itemId;
-    }
-
-
-
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return '{{%image}}';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['filePath', 'itemId', 'modelName', 'urlAlias'], 'required'],
-            [['itemId', 'isMain'], 'integer'],
-            [['name'], 'string', 'max' => 80],
-            [['filePath', 'urlAlias'], 'string', 'max' => 400],
-            [['modelName'], 'string', 'max' => 150]
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => 'ID',
-            'filePath' => 'File Path',
-            'itemId' => 'Item ID',
-            'isMain' => 'Is Main',
-            'modelName' => 'Model Name',
-            'urlAlias' => 'Url Alias',
-        ];
-    }
+	protected function getSubDur(): string
+	{
+		return Inflector::pluralize($this->modelName).'/'.$this->modelName.$this->itemId;
+	}
 }
